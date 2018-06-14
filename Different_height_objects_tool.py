83a729d184979a0c921186055f6ee70dd97411c6
annatg95 <ageorgieva1995@gmail.com>
2018-06-14 08:45:22 +0100

    update

--- Different_height_objects_tool.py
+++ Different_height_objects_tool.py
@@ -5,5 +5,5 @@ selected = cmds.ls(sl=True)
 print len(selected)
 for item in selected:
 	number=random.uniform(0.8,1.0)
-	cmds.move( 0, -1.45, 0,item+'.scalePivot',r=True )
+	cmds.xform(item,cp=True)
 	cmds.setAttr( item+'.scaleY', number )	
